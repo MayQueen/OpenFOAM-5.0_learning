047dac9a9fe51e31a7e0d113a040c91155a722b3
MayQueen <330948319@qq.com>
2021-03-06 15:31:14 +0800

    Delete atmospheric_lidar directory

--- atmospheric_lidar/RM_Hander.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-sys.path.append('C:\\Users\\MERI\\Desktop\\atmospheric_lidar')
-
-import glob
-from raymetrics import ScanningLidarMeasurement
-
-LidarDataObject= r'C:\\Users\\MERI\\Desktop\\20190120\\RM1912000.021110'
-
-RM_file = ScanningLidarMeasurement(file_path)
